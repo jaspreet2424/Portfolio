dd2fec356720f4c935f06ff1df902cd9ca3de5a5
Jaspreet Singh <js9316713287@gmail.com>
2024-02-10 13:02:55 +0530

    Server file removed

--- Server/Routes/User.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerUser , loginUser, checkAuthentication } = require('../Controllers/User');
-const userRouter = express.Router();
-
-userRouter.post('/sign-up' , registerUser);
-userRouter.post('/sign-in' , loginUser);
-userRouter.get('/check-auth' , checkAuthentication);
-
-module.exports = userRouter;
